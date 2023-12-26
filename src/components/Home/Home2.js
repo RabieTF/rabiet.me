c909cf0afd4d0e513330899d6a6942845aca104a
Rabie Toaba <rabie.toa@gmail.com>
2023-12-26 19:03:43 +0100

    Changed artwork, working on home page bg image (unfinished)

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avatar.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -35,7 +35,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} className="img-fluid" width="70%" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
